d478cf4f5f277b68f2e2c62315ff180023846f36
nasasolksp <nasasolksp@gmail.com>
2026-04-21 20:55:38 +0100

    Wiped repo for remake

--- js/employee-credentials.js
+++
@@ -1,9 +0,0 @@
-// Edit this list to change the shared staff login.
-// This is a static-site browser login, so credentials are stored client-side.
-window.EMPLOYEE_CREDENTIALS = [
-    {
-        username: "saturn.staff",
-        password: "26",
-        displayName: "Saturn Staff"
-    }
-];
